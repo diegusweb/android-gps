08dcfc7efba5b6cf6251e7dd08f1938231b6cadd
Diego Rueda <diego.rueda2@gmail.com>
2015-08-27 23:14:17 -0400

    implement gps sample for locaton

--- app/src/main/java/com/util/diegus/gps_beta/MainActivity.java
+++ app/src/main/java/com/util/diegus/gps_beta/MainActivity.java
@@ -1,16 +1,43 @@
 package com.util.diegus.gps_beta;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView messageTextView;
+    TextView messageTextView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messageTextView = (TextView) findViewById(R.id.message_id);
+        messageTextView2 = (TextView) findViewById(R.id.message_id2);
+
+		/* Use the LocationManager class to obtain GPS locations */
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        MyLocationListener mlocListener = new MyLocationListener();
+        mlocListener.setMainActivity(this);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                (LocationListener) mlocListener);
+
+        messageTextView.setText("LocationListener agregado");
+        messageTextView2.setText("");
     }
 
     @Override
@@ -33,5 +60,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setLocation(Location loc) {
+        //Obtener la direcci—n de la calle a partir de la latitud y la longitud
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+                if (!list.isEmpty()) {
+                    Address address = list.get(0);
+                    messageTextView2.setText("Mi direcci—n es: \n"
+                            + address.getAddressLine(0));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/util/diegus/gps_beta/MyLocationListener.java
@@ -0,0 +1,57 @@
+package com.util.diegus.gps_beta;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by HP on 27/08/2015.
+ */
+public class MyLocationListener implements LocationListener {
+    MainActivity mainActivity;
+
+    public MainActivity getMainActivity() {
+        return mainActivity;
+    }
+
+    public void setMainActivity(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        location.getLatitude();
+        location.getLongitude();
+
+        String info = "MI unicacion antual es: " + "\n latitud = "
+                + location.getLatitude() + "\n Longitud = " + location.getLongitude();
+
+        mainActivity.messageTextView.setText(info);
+        this.mainActivity.setLocation(location);
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        // Este mŽtodo se ejecuta cuando el GPS es desactivado
+        mainActivity.messageTextView.setText("GPS Desactivado");
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        // Este mŽtodo se ejecuta cuando el GPS es activado
+        mainActivity.messageTextView.setText("GPS Activado");
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        // Este mŽtodo se ejecuta cada vez que se detecta un cambio en el
+        // status del proveedor de localizaci—n (GPS)
+        // Los diferentes Status son:
+        // OUT_OF_SERVICE -> Si el proveedor esta fuera de servicio
+        // TEMPORARILY_UNAVAILABLE -> Temp˜ralmente no disponible pero se
+        // espera que este disponible en breve
+        // AVAILABLE -> Disponible
+    }
+
+}
